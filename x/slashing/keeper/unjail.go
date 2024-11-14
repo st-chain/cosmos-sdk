3b39a11c01c129fe26e526cdbf813eee0990d9f7
aris <ningxin1718@gmail.com>
2024-11-14 15:22:25 +0800

    fix: unjail

--- x/slashing/keeper/unjail.go
+++ x/slashing/keeper/unjail.go
@@ -2,7 +2,6 @@ package keeper
 
 import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 	"github.com/cosmos/cosmos-sdk/x/slashing/types"
 )
 
@@ -15,18 +14,18 @@ func (k Keeper) Unjail(ctx sdk.Context, validatorAddr sdk.ValAddress) error {
 	}
 
 	// cannot be unjailed if no self-delegation exists
-	selfDel := k.sk.Delegation(ctx, sdk.AccAddress(validatorAddr), validatorAddr)
-	if selfDel == nil {
-		return types.ErrMissingSelfDelegation
-	}
+	//selfDel := k.sk.Delegation(ctx, sdk.AccAddress(validatorAddr), validatorAddr)
+	//if selfDel == nil {
+	//	return types.ErrMissingSelfDelegation
+	//}
 
-	tokens := validator.TokensFromShares(selfDel.GetShares()).TruncateInt()
-	minSelfBond := validator.GetMinSelfDelegation()
-	if tokens.LT(minSelfBond) {
-		return sdkerrors.Wrapf(
-			types.ErrSelfDelegationTooLowToUnjail, "%s less than %s", tokens, minSelfBond,
-		)
-	}
+	//tokens := validator.TokensFromShares(selfDel.GetShares()).TruncateInt()
+	//minSelfBond := validator.GetMinSelfDelegation()
+	//if tokens.LT(minSelfBond) {
+	//	return sdkerrors.Wrapf(
+	//		types.ErrSelfDelegationTooLowToUnjail, "%s less than %s", tokens, minSelfBond,
+	//	)
+	//}
 
 	// cannot be unjailed if not jailed
 	if !validator.IsJailed() {
